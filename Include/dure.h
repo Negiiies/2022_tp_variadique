406b82448e102de324b4e783f9db365ad769eb8f
LE Dylan <dylan.le@ecole-89.com>
2022-12-15 14:55:53 +0100

    update

--- Include/dure.h
+++ Include/dure.h
@@ -6,4 +6,7 @@
 #include <stdarg.h>
 int stu_vputs(unsigned int nbr, ...);
 unsigned int stu_strlen(const char *str);
+int get_digit(int nb, int index);
+int nb_len(int nb);
+int print_base10(int nb);
 #endif /*DURE_H_*/
